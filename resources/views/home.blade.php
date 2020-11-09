0acf7fc9aebe3bc98d833d4ee08b7a3e573e9af5
Furia10-A <70809214+Furia10-A@users.noreply.github.com>
2020-11-09 11:20:02 -0600

    prueba 9

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -108,6 +108,7 @@
                         </div>
                         <div class="card-footer">
                           © 2019-<?php echo date("Y"); ?> - Software de facturacion FR10. |
+                          <a target="_blank" href="https://api.whatsapp.com/send?phone=50660866447">WhatsApp<img height=25px; src='/images/whatsapp.png' border="0"/></a>
                         </div>
                     </div>
                 </div>
